b67b1fbae5b2786aeeee3683603e6c44da343da7
Henrique Monteiro <henriquemonteiro942@gmail.com>
2022-04-28 01:38:27 -0300

    Desafio finalizado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,74 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User does not exists!" });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) {
+    next();
+  }
+
+  if (!user.pro && user.todos.length + 1 <= 10) {
+    next();
+  }
+
+  return response.status(403).json({ error: "Update your plan for to create more todos!" });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id: todoId } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User does not exists!" });
+  }
+
+  const todoIdIsUUID = validate(todoId);
+
+  if (!todoIdIsUUID) {
+    return response.status(400).json({ error: "Todo ID is invalid!" });
+  }
+
+  const todo = user.todos.find(todo => todo.id === todoId);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo does not exists!" });
+  }
+
+  request.user = user;
+
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "User does not exists!" });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
